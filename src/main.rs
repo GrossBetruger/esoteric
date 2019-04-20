f29717bc82ebf6b80f552020c75c4cc4aa0414ec
Oren Koriat <oren@cynerio.co>
2019-04-20 17:53:51 +0300

    print sha256 of file, use digest as path, size 25 MB

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use rand::Rng;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+
 use std::io::prelude::*;
 use std::fs::File;
 
 fn main() {
-    let MB = 20;
+    let MB = 25;
     let size = (MB * 2_u32.pow(20));
     let random_bytes: Vec<u8> = (0..size).map(|_| { rand::random::<u8>() }).collect();
-    let mut buffer = File::create("foo.txt").expect("failed to create file");
 
+    let mut hasher = Sha256::new();
+    hasher.input(&random_bytes);
+    let hex_digest = hasher.result_str();
+    let mut buffer = File::create(&hex_digest).expect("failed to create file");
+    println!("{}", hex_digest);
     buffer.write(&random_bytes).expect("failed to wrtie to file");
 }
