658983e8d431c238827aa9336be4a3bcd56b94b0
Oren Koriat <oren@cynerio.co>
2019-04-21 13:33:44 +0300

    changed random number generation to pseudo random (supposedly faster)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use rand::Rng;
+extern crate xorshift;
+extern crate time;
+
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 
-use std::io::prelude::*;
 use std::fs::File;
+use xorshift::{Rand, Rng, SeedableRng, SplitMix64, Xoroshiro128, Xorshift128, Xorshift1024};
+use time::precise_time_ns;
+use std::io::prelude::*;
+
 
 fn main() {
+    let now = precise_time_ns();
+    // Manually seed a Xorshift128+ PRNG
+    let states = [now, now];
+    let mut rng: Xorshift128 = SeedableRng::from_seed(&states[..]);
+
     let MB = 25;
     let size = (MB * 2_u32.pow(20));
-    let random_bytes: Vec<u8> = (0..size).map(|_| { rand::random::<u8>() }).collect();
+    let random_bytes: Vec<u8> = (0..size).map(|_| { rng.next_u64() as u8 }).collect();
+
 
     let mut hasher = Sha256::new();
     hasher.input(&random_bytes);
